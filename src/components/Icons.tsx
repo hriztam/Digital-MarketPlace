1bd383bc2bb3d5e142585c24949f1ca8c9dc867e
hriztam <hritamstark05@gmail.com>
2024-01-30 01:30:01 +0530

    Updated the Navbar

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -4,8 +4,8 @@ export const Icons = {
   logo: (props: LucideProps) => (
     <svg
       {...props}
-      height="60px"
-      width="60px"
+      height="50px"
+      width="50px"
       viewBox="0 0 128 128"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
