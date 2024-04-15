41334abdbc8d936a140c25e945758c3a202e6224
hriztam <hritamstark05@gmail.com>
2024-04-15 18:01:44 +0530

    Completed the model of the data

--- src/collections/ProductFile.ts
+++ src/collections/ProductFile.ts
@@ -7,35 +7,55 @@ const addUser: BeforeChangeHook = ({ req, data }) => {
   return { ...data, user: user?.id };
 };
 
-// const yourOwnAndPurchased: Access = async ({ req }) => {
-//   const user = req.user as User | null;
+const yourOwnAndPurchased: Access = async ({ req }) => {
+  const user = req.user as User | null;
 
-//   if (user?.role === "admin") return true;
+  if (user?.role === "admin") return true;
 
-//   if (!user) return false;
+  if (!user) return false;
 
-//   const { docs: products } = await req.payload.find({
-//     collection: "products",
-//     depth: 0,
-//     where: {
-//       user: {
-//         equals: user.id,
-//       },
-//     },
-//   });
+  const { docs: products } = await req.payload.find({
+    collection: "products",
+    depth: 0,
+    where: {
+      user: {
+        equals: user.id,
+      },
+    },
+  });
 
-//   const ownProductFileIds = products.map((prod) => prod.product_files).flat();
+  const ownProductFileIds = products.map((prod) => prod.product_files).flat();
 
-//   const { docs: orders } = await req.payload.find({
-//     collection: "orders",
-//     depth: 2,
-//     where: {
-//       user: {
-//         equals: user.id,
-//       },
-//     },
-//   });
-// };
+  const { docs: orders } = await req.payload.find({
+    collection: "orders",
+    depth: 2,
+    where: {
+      user: {
+        equals: user.id,
+      },
+    },
+  });
+
+  const purchasedProductFileIds = orders
+    .map((order) => {
+      return order.products.map((product) => {
+        if (typeof product === "string")
+          return req.payload.logger.error("Product type is String!!");
+
+        return typeof product.product_files === "string"
+          ? product.product_files
+          : product.product_files.id;
+      });
+    })
+    .filter(Boolean)
+    .flat();
+
+  return {
+    id: {
+      in: [...ownProductFileIds, ...purchasedProductFileIds],
+    },
+  };
+};
 
 export const ProductFiles: CollectionConfig = {
   slug: "product_files",
@@ -46,7 +66,9 @@ export const ProductFiles: CollectionConfig = {
     beforeChange: [addUser],
   },
   access: {
-    // read: yourOwnAndPurchased,
+    read: yourOwnAndPurchased,
+    update: ({ req }) => req.user.role === "admin",
+    delete: ({ req }) => req.user.role === "admin",
   },
   upload: {
     staticURL: "/product_files",
