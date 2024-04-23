f67e6a9c319955b839c35d1e87d5bcfd6200446d
hriztam <hritamstark05@gmail.com>
2024-04-23 16:54:34 +0530

    Payment Statusb Component Completed

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,8 +1,9 @@
+import PaymentStatus from "@/components/PaymentStatus";
 import { PRODUCT_CATEGORIES } from "@/config";
 import { getPayloadClient } from "@/get-payload";
 import { getServerSideUser } from "@/lib/payload-users";
 import { formatPrice } from "@/lib/utils";
-import { Product, ProductFile } from "@/payload-types";
+import { Product, ProductFile, User } from "@/payload-types";
 import { cookies } from "next/headers";
 import Image from "next/image";
 import Link from "next/link";
@@ -154,6 +155,12 @@ const ThankYouPage = async ({ searchParams }: PageProps) => {
                 </div>
               </div>
             </div>
+
+            <PaymentStatus
+              isPaid={order._isPaid}
+              orderEmail={(order.user as User).email}
+              orderId={order.id}
+            />
 
             <div className="mt-16 border-t border-gray-200 py-6 text-right">
               <Link
